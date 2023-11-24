ad667f16ec8ceb9c35529308a19a41095cafdb29
Jose Rodiguez <joserodriguezxdxd01@gmail.com>
2023-11-23 19:20:21 -0600

    Merge https://github.com/Agdrael/pruebaaa

--- template/login.php
+++ template/login.php
@@ -1,25 +1,23 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/login.css">
     <title>Sistema de Hospital</title>
 </head>
+
 <body>
-    <form method="post" id="registroForm">
+    <!-- Formulario de inicio de sesión -->
+    <form method="post" id="loginForm" class="active-form">
         <div class="container">
             <table class="login-table">
-                <!-- Contenido de la tabla de inicio de sesión -->
                 <tr>
                     <td colspan="2"><img src="../imagenes2/Hospital Divina providencia.gif" height="300" alt="Logo de Iniciar Sesión"></td>
                 </tr>
                 <tr>
                     <td colspan="2"><h2>Iniciar Sesión</h2></td>
-                   
-                </tr>
-                <tr>
-                    
                 </tr>
                 <tr>
                     <td><label for="username">Usuario:</label></td>
@@ -40,43 +38,37 @@
                     ?>
                 </tr>
                 <tr>
-                    <td colspan="2"><span class="toggle-modal" onclick="openModal()">¿No tienes una cuenta?</span></td>
+                    <td colspan="2"><span class="toggle-modal" onclick="toggleForm()">¿No tienes una cuenta?</span></td>
                 </tr>
             </table>
+        </div>
     </form>
-    <form method="post" id="registroForm">
-            <!-- Ventana modal -->
-            <div id="myModal" class="modal">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h2>¿No tienes una cuenta?</h2>
-                        <span class="modal-close" onclick="closeModal()">×</span>
-                    </div>
-                    <table class="account-check-table">
-                        <!-- Contenido de la tabla de verificación de cuenta -->
-                        <tr>
-                            <td colspan="2"><img src="../imagenes2/Hospital Divina providencia.gif"height="300"  alt="Logo de ¿Tiene una cuenta?"></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><h2>¿No tiene una cuenta?</h2></td>
-                        </tr>
-                        <tr>
-                            <td><label for="check-username">Ingrese su usuario:</label></td>
-                            <td><input type="text" id="check-username" name="check-username"></td>
-                        </tr>
-                        <tr>
-                            <td><label for="check-password">Ingrese su Contraseña:</label></td>
-                            <td>
-                                <input type="password" id="check-password" name="check-password">
-                                <span class="toggle-password" onclick="togglePassword('check-password')">Mostrar Contraseña</span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><input name="btncrear" type="submit" value="Crear Cuenta"></td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+
+    <!-- Formulario de registro de cuenta -->
+    <form method="post" id="registroForm" class="hidden-form">
+        <div class="container">
+            <table class="login-table">
+                <tr>
+                    <td colspan="2"><img src="../imagenes2/Hospital Divina providencia.gif" height="300" alt="Logo de Registro"></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><h2>Registrar Usuario</h2></td>
+                </tr>
+                <tr>
+                    <td><label for="newUsername">Nuevo usuario:</label></td>
+                    <td><input type="text" id="newUsername" name="newUsername"></td>
+                </tr>
+                <tr>
+                    <td><label for="newPassword">Contraseña:</label></td>
+                    <td>
+                        <input type="password" id="newPassword" name="newPassword">
+                        <span class="toggle-password" onclick="togglePassword('newPassword')">Mostrar Contraseña</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" value="Registrar Usuario"></td>
+                </tr>
+            </table>
         </div>
     </form>
 
@@ -87,23 +79,16 @@
             passwordInput.setAttribute("type", type);
         }
 
-        function openModal() {
-            var modal = document.getElementById("myModal");
-            modal.style.display = "flex";
-        }
+        function toggleForm() {
+            var loginForm = document.getElementById("loginForm");
+            var registroForm = document.getElementById("registroForm");
 
-        function closeModal() {
-            var modal = document.getElementById("myModal");
-            modal.style.display = "none";
-        }
-
-        // Cierra la ventana modal si el usuario hace clic fuera de ella
-        window.onclick = function(event) {
-            var modal = document.getElementById("myModal");
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
+            loginForm.classList.toggle("hidden-form");
+            loginForm.classList.toggle("active-form");
+            registroForm.classList.toggle("hidden-form");
+            registroForm.classList.toggle("active-form");
         }
     </script>
 </body>
+
 </html>
